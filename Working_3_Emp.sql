84fda8236d35f4920c563040ab50e713ac60d6ab
Prasanna <prasanna@hexaware.com>
2018-11-05 16:00:10 +0530

    Emp Added

---
+++ Working_3_Emp.sql
@@ -0,0 +1,18 @@
+Create Table Emp
+(
+    Empno int AUTO_INCREMENT,
+    Name varchar(50), 
+    Dept varchar(30),
+    Desig varchar(30),
+    Basic INT,
+    primary key(Empno)
+)
+
+
+
+Insert into Emp(Name,Dept,Desig,Basic) values
+('kiran kumar kailasam','java','developer',42233),
+('Vishnu Vardhan Reddy','dotnet','programmer',42133),
+('Ram Kishan','java','developer',82233),
+('Kareem','dotnet','programmer',52234),
+('Sheik Sulthan Mohammad','dotnet','developer',42555)
